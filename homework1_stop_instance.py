16346d0b828a8b0fb706aacc7667ee45cb59df48
nigorasultonov <152802393+nigorasultonov@users.noreply.github.com>
2023-12-18 22:42:33 -0500

    Create homework1_stop_instance.py
    python code for lambda

---
+++ homework1_stop_instance.py
@@ -0,0 +1,11 @@
+import boto3
+
+region = 'us-east-2'
+instance_id = aws_instance.ansible.id 
+
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=[instance_id])
+    print(f"Instance {instance_id} stopped")
+    
