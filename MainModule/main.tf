68534dc9b47181567311c8e06e19131ffb180cd3
nigorasultonov <152802393+nigorasultonov@users.noreply.github.com>
2024-02-28 17:10:30 -0500

    Create main.tf

---
+++ MainModule/main.tf
@@ -0,0 +1,11 @@
+# main_module/main.tf
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instances" {
+  source = "./EC2_Module"
+
+  number_of_instances = 3 # Default number of instances
+}
